47c38353e4e0fe04942fdd56efb6572f896deb34
simonmartyr <simartyr@googlemail.com>
2017-08-17 00:10:53 +0200

    Fist couple of tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,20 @@ var gulp = require('gulp');
 
 // Styles
 gulp.task('styles', function(){
-  console.log("starting styles task");
-  
+  console.log("Starting styles task...");
+
 });
 
-
 // Scripts
+gulp.task('scripts', function(){
+  console.log("Starting scripts task...");
+});
 
 // Images
+gulp.task('images', function(){
+  console.log("Starting images task...");
+});
+
+gulp.task('default', function(){
+  console.log("starting default task...");
+});
