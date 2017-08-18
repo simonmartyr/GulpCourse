592de299f0e84dbe00f0718e5c461d5022710225
simonmartyr <simartyr@googlemail.com>
2017-08-18 22:09:24 +0200

    css minify and concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
 var pump = require('pump');
 var livereload = require('gulp-livereload');
 
 // File Paths
+var DIST_PATH = 'public/dist';
 var SCRIPTS_PATH = 'public/scripts/**/*.js';
+var CSS_PATH = 'public/css/**/*.css';
 
 
 // Styles
 gulp.task('styles', function(){
   console.log("Starting styles task...");
+  pump([
+    gulp.src(['public/css/reset.css', CSS_PATH]),
+    concat('styles.css'),
+    minifyCss(),
+    gulp.dest(DIST_PATH),
+    livereload()
+  ]);
 });
 
 // Scripts
@@ -18,10 +29,9 @@ gulp.task('scripts', function(){
   pump([
         gulp.src(SCRIPTS_PATH),
         uglify(),
-        gulp.dest('public/dist'),
+        gulp.dest(DIST_PATH),
         livereload()
-    ]
-  );
+    ]);
 });
 
 // Images
@@ -34,6 +44,7 @@ gulp.task('watch', function(){
   require('./server.js');
   livereload.listen(); 
   gulp.watch(SCRIPTS_PATH, ['scripts']);
+  gulp.watch(CSS_PATH, ['styles']);
 })
 
 gulp.task('default', function(){
